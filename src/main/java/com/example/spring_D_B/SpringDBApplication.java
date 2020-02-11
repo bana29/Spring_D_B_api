c7c3c6b18ce1e1ca7aec323a743aff7ac25aede6
Dieneba banan DIARRA <dieyna93@gmail.com>
2020-02-11 11:00:09 +0100

    first commit

---
+++ src/main/java/com/example/spring_D_B/SpringDBApplication.java
@@ -0,0 +1,14 @@
+package com.example.spring_D_B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDBApplication.class, args);
+		System.out.println("Helloword");
+	}
+
+}
